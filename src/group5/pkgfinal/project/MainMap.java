10cb31725e93cbf0e11703ebca0ad413db624ed7
Theodore Nguyen <tzn5100@gmail.com>
2020-10-29 14:29:27 -0400

    Intersection
    Allows intersection to move to new map

--- src/group5/pkgfinal/project/MainMap.java
+++ src/group5/pkgfinal/project/MainMap.java
@@ -10,6 +10,8 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
@@ -20,7 +22,7 @@ import javax.swing.JPanel;
  *
  * @author theodore
  */
-public class MainMap extends JPanel implements KeyListener {
+public class MainMap extends JPanel implements KeyListener,ActionListener {
 
     //back to the menu button
     JButton backButton;
@@ -35,19 +37,31 @@ public class MainMap extends JPanel implements KeyListener {
             berksGame,
             scrantonGame,
             fayetteGame;
+    
+    //Rectangles for intersection. Used for when the player intersects with the campus.
+    Rectangle univParkRctngle, wrldCmpRctngle, brksRctngle, scrantonRctngle, fytteRctngle;
 
     //adds the player
     JButton player;
-    int vertical,
-        horizontal;
-
-    Games games;
+    Rectangle playerRctngle;
+    int vertical, horizontal;
+     
+    //Game Classes
+    UniversityParkGame universityPark;
 
     //constructor
     public MainMap() {
 
         //sets a nul layout to add the buttons 
         super();
+        InitialSetUpForMainMap();
+        CreateGamesThatWillBeSwapped();
+
+    }
+    
+    //The Initial Setup when the player goes to the Main Map
+    public void InitialSetUpForMainMap(){
+                //sets a nul layout to add the buttons 
         setBackground(Color.white);
         setLayout(null);
 
@@ -56,13 +70,11 @@ public class MainMap extends JPanel implements KeyListener {
         add(backButton);
         backButton.setBounds(new Rectangle(500, 10, 300, 30));
 
-        //Games to be added to map
-        Games games = new Games();
 
         //Player
         player = new JButton();
         add(player);
-        player.setBounds(new Rectangle(0, 0, 70, 100));
+        player.setBounds(new Rectangle(horizontal, vertical, 70, 100));
 
         //University Park
         univParkGame = new JButton("UniversityPark");
@@ -72,6 +84,13 @@ public class MainMap extends JPanel implements KeyListener {
         setFocusable(true);
         addKeyListener(this);
         requestFocusInWindow();
+    }
+    
+    //Put your Game Class here so they can be used to switch between panels
+    public void CreateGamesThatWillBeSwapped() {
+        universityPark = new UniversityParkGame();
+        universityPark.backToMap.addActionListener(this);//adds a back button to return to the main menu
+
     }
 
     //Draws the penn state map
@@ -87,38 +106,60 @@ public class MainMap extends JPanel implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         int k = e.getKeyCode();
-
+        
+        univParkRctngle = univParkGame.getBounds();
+        playerRctngle = player.getBounds();
+        
         //move left
         if (k == e.VK_LEFT) {
-            System.out.println("left");
             horizontal -= 30;
-            player.setBounds(horizontal,vertical, 70, 100);
+            player.setBounds(horizontal, vertical, 70, 100);
         }
 
         //move right
         if (k == e.VK_RIGHT) {
-            System.out.println("right");
             horizontal += 30;
             player.setBounds(horizontal, vertical, 70, 100);
         }
 
         //move up
         if (k == e.VK_UP) {
-            System.out.println("up");
             vertical -= 30;
-            player.setBounds(horizontal,vertical, 70, 100);
+            player.setBounds(horizontal, vertical, 70, 100);
         }
 
         //move down
         if (k == e.VK_DOWN) {
-            System.out.println("down");
             vertical += 30;
             player.setBounds(horizontal, vertical, 70, 100);
         }
+        
+        //Enters the University Park Game Map.
+        if (playerRctngle.intersects(univParkRctngle)){
+            removeAll();
+            add(universityPark);
+            validate();
+            repaint();
+            
+        }
+
+        
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent eventMap) {
+        Object objMap = eventMap.getSource();
+        
+            if (objMap == universityPark.backToMap) {
+            removeAll();
+            InitialSetUpForMainMap(); //rebuild the Map.
+            validate();
+            repaint();
+        }
     }
 
 }
